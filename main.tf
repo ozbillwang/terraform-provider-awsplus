613ba0605e0e8cf10525d12273b74d7d56185a71
Bill Wang <bill.wang@myob.com>
2017-04-21 22:03:17 +1000

    add fmt check

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
-#providers {
-#  awsplus="release/terraform-provider-awsplus-darwin-amd64"
-#}
-
 resource "awsplus_vpc_peering_accept_all" "MyPeering" {
   accepter   = "123456789"
   requester  = "987654321"
